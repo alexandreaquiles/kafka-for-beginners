fbe0f10bd93b43bfca846e555927ab55d16876a1
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-04-21 17:43:50 -0300

    Document auto.offset.reset

--- src/main/java/br/com/alexandreaquiles/kafka/Consumer.java
+++ src/main/java/br/com/alexandreaquiles/kafka/Consumer.java
@@ -51,71 +51,43 @@ class ConsumerRunnable implements  Runnable {
     properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     // no group id
-    // properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "meu-grupo");
-    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "meu-novo-grupo");
+    //properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+    /*
+    auto.offset.reset
+
+    Por padrão, um novo grupo não obtém as mensagens do começo.
+
+    What to do when there is no initial offset in Kafka or if the current offset does not exist any more on the server
+      (e.g. because that data has been deleted):
+
+    earliest: automatically reset the offset to the earliest offset
+    latest: automatically reset the offset to the latest offset
+    none: throw exception to the consumer if no previous offset is found for the consumer's group
+    anything else: throw exception to the consumer.
+
+    Type:	string
+    Default:	latest
+    Valid Values:	[latest, earliest, none]
+    Importance:	medium
+
+     */
 
     consumer = new KafkaConsumer<>(properties);
 
-    // don't subscribe
-    // consumer.subscribe(Collections.singleton("first-topic"));
+     consumer.subscribe(Collections.singleton("first-topic"));
 
     // assign and seek => replay data or fetch a specific message
-    TopicPartition topicPartition = new TopicPartition("first-topic", 0);
-    consumer.assign(Collections.singleton(topicPartition));
+    //TopicPartition topicPartition = new TopicPartition("first-topic", 0);
+    //consumer.assign(Collections.singleton(topicPartition));
 
-    consumer.seek(topicPartition, 10L);
-
-    /*
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 10 timestamp: 1587492199732
-    key: null value: Oi!
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 11 timestamp: 1587493645862
-    key: null value: oi
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 12 timestamp: 1587497580686
-    key: id_1 value: Mensagem direto do Java!
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 13 timestamp: 1587497580686
-    key: id_3 value: Mensagem direto do Java!
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 14 timestamp: 1587497580686
-    key: id_6 value: Mensagem direto do Java!
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 15 timestamp: 1587498761729
-    key: id_1 value: Mensagem direto do Java: 1
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 16 timestamp: 1587498761729
-    key: id_3 value: Mensagem direto do Java: 3
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 17 timestamp: 1587498761733
-    key: id_6 value: Mensagem direto do Java: 6
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 18 timestamp: 1587499393338
-    key: null value: Parece que não...
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 19 timestamp: 1587500433690
-    key: null value: Será que funciona?
-
-    [Thread-0] INFO br.com.alexandreaquiles.kafka.ConsumerRunnable - topic: first-topic partition: 0 offset: 20 timestamp: 1587500464140
-    key: null value: vamos testar isso daqui
-     */
+    //consumer.seek(topicPartition, 10L);
 
     /*
     Same as
 
     kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first-topic --partition 0 --offset 10
-
-    Oi!
-    oi
-    Mensagem direto do Java!
-    Mensagem direto do Java!
-    Mensagem direto do Java!
-    Mensagem direto do Java: 1
-    Mensagem direto do Java: 3
-    Mensagem direto do Java: 6
-    Parece que não...
-    Será que funciona?
-    vamos testar isso daqui
 
      */
 
