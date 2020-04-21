ea99f92f86d92a928b2edd87fea830fd7b3f8d5e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-04-21 17:25:15 -0300

    Consumer usando Threads

--- src/main/java/br/com/alexandreaquiles/kafka/Consumer.java
+++ src/main/java/br/com/alexandreaquiles/kafka/Consumer.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +12,38 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class Consumer {
 
   private static final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
+
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    ConsumerRunnable runnable = new ConsumerRunnable(countDownLatch);
+    Thread thread = new Thread(runnable);
+    thread.start();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+      logger.info("Vai desligar...");
+      runnable.shutdown();
+    }));
+
+    countDownLatch.await();
+
+  }
+}
+
+class ConsumerRunnable implements  Runnable {
+
+  private static final Logger logger = LoggerFactory.getLogger(ConsumerRunnable.class);
+  private final KafkaConsumer<String, String> consumer;
+  private CountDownLatch countDownLatch;
+
+  ConsumerRunnable(CountDownLatch countDownLatch) {
+    this.countDownLatch = countDownLatch;
     // http://kafka.apache.org/documentation/#consumerconfigs
     Properties properties = new Properties();
     properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -25,26 +52,41 @@ public class Consumer {
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "meu-grupo");
     properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-    KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+    consumer = new KafkaConsumer<>(properties);
 
     consumer.subscribe(Collections.singleton("first-topic"));
+  }
 
-    while (true) {
-      // consumer.poll(100);
-      ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-      for (ConsumerRecord<String, String> record : records) {
-        logger.info(
-              "topic: " + record.topic() + " "
-            + "partition: " + record.partition() + " "
-            + "offset: " + record.offset() + " "
-            + "timestamp: " + record.timestamp() + "\n"
-            + "key: " + record.key() + " "
-            + "value: " + record.value() + "\n"
-        );
+  @Override
+  public void run() {
+    try {
 
+      while (true) {
+        // consumer.poll(100);
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        for (ConsumerRecord<String, String> record : records) {
+          logger.info(
+            "topic: " + record.topic() + " "
+              + "partition: " + record.partition() + " "
+              + "offset: " + record.offset() + " "
+              + "timestamp: " + record.timestamp() + "\n"
+              + "key: " + record.key() + " "
+              + "value: " + record.value() + "\n"
+          );
+        }
       }
 
-
+    } catch ( WakeupException ex) {
+      logger.info("Desligando...");
+      consumer.close();
+      countDownLatch.countDown();
     }
+
+
+  }
+
+  public void shutdown() {
+    logger.info("Consumer is waking up...");
+    consumer.wakeup();
   }
 }
