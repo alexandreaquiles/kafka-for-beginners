97bac307c11401bbdd540f77c9754d1204273674
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-04-21 16:26:43 -0300

    Callback when producing record to topic

--- src/main/java/br/com/alexandreaquiles/kafka/Producer.java
+++ src/main/java/br/com/alexandreaquiles/kafka/Producer.java
@@ -1,14 +1,17 @@
 package br.com.alexandreaquiles.kafka;
 
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
 public class Producer {
+
+  private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+
   public static void main(String[] args) {
     // http://kafka.apache.org/documentation/#producerconfigs
     Properties properties = new Properties();
@@ -23,7 +26,19 @@ public class Producer {
 
     ProducerRecord record = new ProducerRecord<>("first-topic", "Mensagem direto do Java!");
 
-    producer.send(record);
+    producer.send(record, (recordMetadata, e) -> {
+      if (e == null) {
+        logger.info(
+          "topic: " + recordMetadata.topic() + "\n"
+            + "partition: " + recordMetadata.partition() + "\n"
+            + "offset: " + recordMetadata.offset() + "\n"
+            + "timestamp: " + recordMetadata.timestamp() + "\n"
+        );
+      } else {
+        logger.error("Error while producing", e);
+      }
+
+    });
 
     //producer.flush();
     producer.close();
